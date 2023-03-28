6b38bc64410bcca199f553d5d278434443f1f15e
Maksym Pecherytsia <findimrk@gmail.com>
2023-03-28 12:39:41 +0300

    added layout on grid and footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,34 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import { Htag } from "../components";
+import React, { useEffect, useState } from "react";
+import { Htag, Button, Ptag, Tag, Rating } from "@/components";
+import { Layout, withLayout } from "@/layout/Layout";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
+	const [rating, setRating] = useState<number>(4);
 	return (
 		<>
-			<div>
-				<Htag tag={"h1"}>Lorem ipsum dolor sit amet consectetur</Htag>
-				<Htag tag={"h2"}>Lorem ipsum dolor sit amet consectetur</Htag>
-				<Htag tag={"h3"}>Lorem ipsum dolor sit amet consectetur</Htag>
-			</div>
+			<Htag tag={"h1"}>"counter"</Htag>
+			<Button appearance="primary">Button</Button>
+			<br />
+			<Button appearance="ghost" arrow="down">
+				Button
+			</Button>
+			<Ptag size={"l"}>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus vitae
+				consequatur obcaecati similique atque neque molestias repellendus,
+			</Ptag>
+			<Tag size={"s"} color={"ghost"}>
+				Small ghost
+			</Tag>
+			<Tag size={"s"} color={"gray"}>
+				Small gray
+			</Tag>
+			<Tag size={"s"} color={"red"}>
+				Small red
+			</Tag>
+			<Rating rating={rating} />
+			<Rating rating={rating} isEditable={true} setRating={setRating} />
 		</>
 	);
 }
+
+export default withLayout(Home);
